1ccd1ef85463021cd1dd29b2409d518016b55df0
huzaifa-aziz <azizh4495@gmail.com>
2024-02-08 02:23:09 -0800

    first commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'facebook',
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.message),
+              onPressed: () {},
+            )
+          ],
+          bottom: TabBar(
+              unselectedLabelColor: Colors.black54,
+              indicatorColor: Colors.blue,
+              labelColor: Colors.blue,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home),
+                ),
+                // Tab(
+                //   icon: (Icons.home),
+                // ),
+                // Tab(
+                //   icon: (CupertinoIcons.home),
+                // ),
+                // Tab(
+                //   icon: (CupertinoIcons.home),
+                // ),
+                // Tab(
+                //   icon: (CupertinoIcons.home),
+                // ),
+              ]),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(CupertinoIcons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+            Icon(Icons.home),
+          ],
+        ),
+      ),
+    );
+  }
+}
